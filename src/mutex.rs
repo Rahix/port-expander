588a9d28e563bd63be29785dfc1c8548f10d17b7
Rahix <rahix@rahix.de>
2024-02-15 19:49:03 +0100

    Define our own mutex trait
    Right now, we are still relying on `shared_bus::BusMutex`.  As
    `shared-bus` has gotten obsolete with the release of `embedded-hal` 1.0,
    it's time to drop our dependency on it.  To do this, we need a
    replacement for `BusMutex`.  As there isn't really any other crate
    providing something suitable, let's roll our own - exactly what
    `shared-bus` did in its time as well.

---
+++ src/mutex.rs
@@ -0,0 +1,54 @@
+/// Common interface for mutex implementations.
+///
+/// `port-expander` needs a mutex to ensure only a single pin object can access the port-expander at the same time,
+/// in concurrent situations.  `port-expander` already implements this trait for a number of existing
+/// mutex types.  Most of them are guarded by a feature that needs to be enabled.  Here is an
+/// overview:
+///
+/// | Mutex | Feature Name | Notes |
+/// | --- | --- | --- |
+/// | [`core::cell::RefCell`] | _always available_ | For sharing within a single execution context. |
+///
+/// For other mutex types, a custom implementation is needed.  Due to the orphan rule, it might be
+/// necessary to wrap it in a newtype.  As an example, this is what such a custom implementation
+/// might look like:
+///
+/// ```
+/// struct MyMutex<T>(std::sync::Mutex<T>);
+///
+/// impl<T> port_expander::PortMutex for MyMutex<T> {
+///     type Port = T;
+///
+///     fn create(v: T) -> Self {
+///         Self(std::sync::Mutex::new(v))
+///     }
+///
+///     fn lock<R, F: FnOnce(&mut Self::Port) -> R>(&self, f: F) -> R {
+///         let mut v = self.0.lock().unwrap();
+///         f(&mut v)
+///     }
+/// }
+/// ```
+pub trait PortMutex {
+    /// The actual port-expander that is wrapped inside this mutex.
+    type Port;
+
+    /// Create a new mutex of this type.
+    fn create(v: Self::Port) -> Self;
+
+    /// Lock the mutex and give a closure access to the port-expander inside.
+    fn lock<R, F: FnOnce(&mut Self::Port) -> R>(&self, f: F) -> R;
+}
+
+impl<T> PortMutex for core::cell::RefCell<T> {
+    type Port = T;
+
+    fn create(v: Self::Port) -> Self {
+        core::cell::RefCell::new(v)
+    }
+
+    fn lock<R, F: FnOnce(&mut Self::Port) -> R>(&self, f: F) -> R {
+        let mut v = self.borrow_mut();
+        f(&mut v)
+    }
+}
