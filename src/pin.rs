Rahix/port-expander

commit e7712de7745e48f81aefdaec05ec57c7e605d2e0

feat: Update to embedded-hal 1.0.0 update embedded-hal-mock to 0.10.0

src/pin.rs
@@ -1,5 +1,5 @@
 use core::marker::PhantomData;
-use embedded_hal::digital::v2 as hal_digital;
+use embedded_hal::digital::{self as hal_digital, ErrorType};
 
 /// Representation of a port-expander pin.
 ///
@@ -29,11 +29,18 @@ where
         self.pin_mask
     }
 
-    pub(crate) fn port_driver<'b>(&'b self) -> &'b MUTEX {
-        &self.port_driver
+    pub(crate) fn port_driver(&self) -> &MUTEX {
+        self.port_driver
     }
 }
-
+impl<'a, MODE, MUTEX, PD> ErrorType for Pin<'a, MODE, MUTEX>
+where
+    PD: crate::PortDriver + crate::PortDriverTotemPole,
+    PD::Error: embedded_hal::digital::Error,
+    MUTEX: shared_bus::BusMutex<Bus = PD>,
+{
+    type Error = PD::Error;
+}
 impl<'a, MODE, MUTEX, PD> Pin<'a, MODE, MUTEX>
 where
     PD: crate::PortDriver + crate::PortDriverTotemPole,
@@ -121,16 +128,15 @@ where
 
 impl<'a, MODE: crate::mode::HasInput, MUTEX, PD> hal_digital::InputPin for Pin<'a, MODE, MUTEX>
 where
-    PD: crate::PortDriver,
+    PD: crate::PortDriver + crate::PortDriverTotemPole,
+    <PD as crate::PortDriver>::Error: embedded_hal::digital::Error,
     MUTEX: shared_bus::BusMutex<Bus = PD>,
 {
-    type Error = PD::Error;
-
-    fn is_high(&self) -> Result<bool, Self::Error> {
+    fn is_high(&mut self) -> Result<bool, <PD as crate::PortDriver>::Error> {
         Pin::is_high(self)
     }
 
-    fn is_low(&self) -> Result<bool, Self::Error> {
+    fn is_low(&mut self) -> Result<bool, <PD as crate::PortDriver>::Error> {
         Pin::is_low(self)
     }
 }
@@ -178,11 +184,10 @@ where
 
 impl<'a, MODE: crate::mode::HasOutput, MUTEX, PD> hal_digital::OutputPin for Pin<'a, MODE, MUTEX>
 where
-    PD: crate::PortDriver,
+    PD: crate::PortDriver + crate::PortDriverTotemPole,
+    <PD as crate::PortDriver>::Error: embedded_hal::digital::Error,
     MUTEX: shared_bus::BusMutex<Bus = PD>,
 {
-    type Error = PD::Error;
-
     fn set_low(&mut self) -> Result<(), Self::Error> {
         Pin::set_low(self)
     }
@@ -195,25 +200,17 @@ where
 impl<'a, MODE: crate::mode::HasOutput, MUTEX, PD> hal_digital::StatefulOutputPin
     for Pin<'a, MODE, MUTEX>
 where
-    PD: crate::PortDriver,
+    PD: crate::PortDriver + crate::PortDriverTotemPole,
+    <PD as crate::PortDriver>::Error: embedded_hal::digital::Error,
     MUTEX: shared_bus::BusMutex<Bus = PD>,
 {
-    fn is_set_high(&self) -> Result<bool, Self::Error> {
+    fn is_set_high(&mut self) -> Result<bool, Self::Error> {
         Pin::is_set_high(self)
     }
 
-    fn is_set_low(&self) -> Result<bool, Self::Error> {
+    fn is_set_low(&mut self) -> Result<bool, Self::Error> {
         Pin::is_set_low(self)
     }
-}
-
-impl<'a, MODE: crate::mode::HasOutput, MUTEX, PD> hal_digital::ToggleableOutputPin
-    for Pin<'a, MODE, MUTEX>
-where
-    PD: crate::PortDriver,
-    MUTEX: shared_bus::BusMutex<Bus = PD>,
-{
-    type Error = PD::Error;
 
     fn toggle(&mut self) -> Result<(), Self::Error> {
         Pin::toggle(self)

Cargo.toml
@@ -13,8 +13,8 @@ version = "0.4.1"
 edition = "2021"
 
 [dependencies]
-embedded-hal = { version = "0.2.4", features = ["unproven"] }
+embedded-hal = { version = "1.0.0" }
 shared-bus = "0.3.0"
 
 [dev-dependencies]
-embedded-hal-mock = "0.9.0"
+embedded-hal-mock = "0.10.0"

src/bus.rs
@@ -1,15 +1,13 @@
-use embedded_hal::blocking::i2c as hal_i2c;
+use embedded_hal::i2c as hal_i2c;
 
-/// Blanket trait for types implementing `i2c::WriteRead + i2c::Write + i2c::Read`
-pub trait I2cBus: hal_i2c::WriteRead + hal_i2c::Write + hal_i2c::Read {
-    type BusError: From<<Self as hal_i2c::WriteRead>::Error>
-        + From<<Self as hal_i2c::Write>::Error>
-        + From<<Self as hal_i2c::Read>::Error>;
+/// Blanket trait for types implementing `i2c::I2c
+pub trait I2cBus: hal_i2c::I2c {
+    type BusError: From<<Self as hal_i2c::ErrorType>::Error>;
 }
 
 impl<T, E> I2cBus for T
 where
-    T: hal_i2c::WriteRead<Error = E> + hal_i2c::Write<Error = E> + hal_i2c::Read<Error = E>,
+    T: hal_i2c::I2c<Error = E>,
 {
     type BusError = E;
 }

src/dev/max7321.rs
@@ -22,7 +22,7 @@ where
         )))
     }
 
-    pub fn split<'a>(&'a mut self) -> Parts<'a, I2C, M> {
+    pub fn split(&mut self) -> Parts<'_, I2C, M> {
         Parts {
             p0: crate::Pin::new(0, &self.0),
             p1: crate::Pin::new(1, &self.0),
@@ -92,7 +92,7 @@ impl<I2C: crate::I2cBus> crate::PortDriver for Driver<I2C> {
 
 #[cfg(test)]
 mod tests {
-    use embedded_hal_mock::i2c as mock_i2c;
+    use embedded_hal_mock::eh1::i2c as mock_i2c;
 
     #[test]
     fn max7321() {

src/dev/pca9536.rs
@@ -22,7 +22,7 @@ where
         Self(shared_bus::BusMutex::create(Driver::new(i2c)))
     }
 
-    pub fn split<'a>(&'a mut self) -> Parts<'a, I2C, M> {
+    pub fn split(&mut self) -> Parts<'_, I2C, M> {
         Parts {
             io0: crate::Pin::new(0, &self.0),
             io1: crate::Pin::new(1, &self.0),
@@ -136,7 +136,7 @@ impl<I2C: crate::I2cBus> crate::PortDriverPolarity for Driver<I2C> {
 
 #[cfg(test)]
 mod tests {
-    use embedded_hal_mock::i2c as mock_i2c;
+    use embedded_hal_mock::eh1::i2c as mock_i2c;
 
     #[test]
     fn pca9536() {

src/dev/pca9538.rs
@@ -22,7 +22,7 @@ where
         Self(shared_bus::BusMutex::create(Driver::new(i2c, a0, a1)))
     }
 
-    pub fn split<'a>(&'a mut self) -> Parts<'a, I2C, M> {
+    pub fn split(&mut self) -> Parts<'_, I2C, M> {
         Parts {
             io0: crate::Pin::new(0, &self.0),
             io1: crate::Pin::new(1, &self.0),
@@ -148,7 +148,7 @@ impl<I2C: crate::I2cBus> crate::PortDriverPolarity for Driver<I2C> {
 
 #[cfg(test)]
 mod tests {
-    use embedded_hal_mock::i2c as mock_i2c;
+    use embedded_hal_mock::eh1::i2c as mock_i2c;
 
     #[test]
     fn pca9538() {

src/dev/pca9555.rs
@@ -22,7 +22,7 @@ where
         Self(shared_bus::BusMutex::create(Driver::new(i2c, a0, a1, a2)))
     }
 
-    pub fn split<'a>(&'a mut self) -> Parts<'a, I2C, M> {
+    pub fn split(&mut self) -> Parts<'_, I2C, M> {
         Parts {
             io0_0: crate::Pin::new(0, &self.0),
             io0_1: crate::Pin::new(1, &self.0),
@@ -210,7 +210,7 @@ impl<I2C: crate::I2cBus> crate::PortDriverPolarity for Driver<I2C> {
 
 #[cfg(test)]
 mod tests {
-    use embedded_hal_mock::i2c as mock_i2c;
+    use embedded_hal_mock::eh1::i2c as mock_i2c;
 
     #[test]
     fn pca9555() {

src/dev/pcal6408a.rs
@@ -22,7 +22,7 @@ where
         Self(shared_bus::BusMutex::create(Driver::new(i2c, addr)))
     }
 
-    pub fn split<'a>(&'a mut self) -> Parts<'a, I2C, M> {
+    pub fn split(&mut self) -> Parts<'_, I2C, M> {
         Parts {
             io0: crate::Pin::new(0, &self.0),
             io1: crate::Pin::new(1, &self.0),
@@ -115,8 +115,7 @@ impl<I2C: crate::I2cBus> crate::PortDriver for Driver<I2C> {
         out &= !mask_low as u8;
         self.out = Some(out);
         if (mask_high | mask_low) & 0x00FF != 0 {
-            self.i2c
-                .write_reg(self.addr, Regs::OutputPort, (out & 0xFF) as u8)?;
+            self.i2c.write_reg(self.addr, Regs::OutputPort, out)?;
         }
         Ok(())
     }
@@ -185,7 +184,7 @@ impl<I2C: crate::I2cBus> crate::PortDriverPolarity for Driver<I2C> {
 
 #[cfg(test)]
 mod tests {
-    use embedded_hal_mock::i2c as mock_i2c;
+    use embedded_hal_mock::eh1::i2c as mock_i2c;
 
     #[test]
     fn pca6408a() {

commit 91ac7347cc817d48da2fecbf5ed75014272eea6a

Remove a superfluous trait bound that broke non-totem-pole devices
There is no requirement for the hal trait implementations (`InputPin`,
`OutputPin`, `StatefulOutputPin`) to require a `PortDriverTotemPole`.
In fact, the presence of this bound means non-totem-pole devices no
longer have hal trait implementations.

Drop the superfluous bound to make non-totem-pole devices work again.

Fixes: e7712de7745e ("feat: Update to embedded-hal 1.0.0")
Reported-by: Thomas Riedler <tomried@riedler.me>

src/pin.rs
@@ -1,5 +1,5 @@
 use core::marker::PhantomData;
-use embedded_hal::digital::{self as hal_digital, ErrorType};
+use embedded_hal::digital::{self as hal_digital};
 
 /// Representation of a port-expander pin.
 ///
@@ -71,9 +71,9 @@ impl<PDE> From<PDE> for PinError<PDE> {
     }
 }
 
-impl<'a, MODE, MUTEX, PD> ErrorType for Pin<'a, MODE, MUTEX>
+impl<'a, MODE, MUTEX, PD> hal_digital::ErrorType for Pin<'a, MODE, MUTEX>
 where
-    PD: crate::PortDriver + crate::PortDriverTotemPole,
+    PD: crate::PortDriver,
     PD::Error: core::fmt::Debug,
     MUTEX: crate::PortMutex<Port = PD>,
 {
@@ -199,7 +199,7 @@ where
 
 impl<'a, MODE: crate::mode::HasInput, MUTEX, PD> hal_digital::InputPin for Pin<'a, MODE, MUTEX>
 where
-    PD: crate::PortDriver + crate::PortDriverTotemPole,
+    PD: crate::PortDriver,
     <PD as crate::PortDriver>::Error: core::fmt::Debug,
     MUTEX: crate::PortMutex<Port = PD>,
 {
@@ -258,7 +258,7 @@ where
 
 impl<'a, MODE: crate::mode::HasOutput, MUTEX, PD> hal_digital::OutputPin for Pin<'a, MODE, MUTEX>
 where
-    PD: crate::PortDriver + crate::PortDriverTotemPole,
+    PD: crate::PortDriver,
     <PD as crate::PortDriver>::Error: core::fmt::Debug,
     MUTEX: crate::PortMutex<Port = PD>,
 {
@@ -274,7 +274,7 @@ where
 impl<'a, MODE: crate::mode::HasOutput, MUTEX, PD> hal_digital::StatefulOutputPin
     for Pin<'a, MODE, MUTEX>
 where
-    PD: crate::PortDriver + crate::PortDriverTotemPole,
+    PD: crate::PortDriver,
     <PD as crate::PortDriver>::Error: core::fmt::Debug,
     MUTEX: crate::PortMutex<Port = PD>,
 {